831fa2340c1ae460e1029a523982f625a49f387c
BaptisteC-prog <69847031+BaptisteC-prog@users.noreply.github.com>
2020-10-08 10:11:25 +0200

    Add files via upload

--- js/Map.js
+++ js/Map.js
@@ -14,43 +14,29 @@
         center: this.location
      });
 
-     ajaxGet("resto.json", function (reponse) 
-     {
-       let listRestos = JSON.parse(reponse);
-       //console.log(listRestos.restaurants[0].lat);
-       for ( let resto in listRestos.restaurants ) 
-         {
-           let lat=listRestos.restaurants[resto].lat;
-           let long=listRestos.restaurants[resto].long;
-           let latLng = new google.maps.LatLng(lat,long);
-           let marker = new google.maps.Marker({
-               position: latLng,
-               map: map
-             });
-         }
+    }
+
+  ajaxGet(url, callback) {
+    var req = new XMLHttpRequest();
+    req.open("GET", url);
+    req.addEventListener("load", function ()    {
+      if (req.status >= 200 && req.status < 400) 
+      {
+          // Appelle la fonction callback en lui passant la réponse de la requête
+          callback(req.responseText);
+      } else { console.error(req.status + " " + req.statusText + " " + url); }
     });
-
-  }
-
-
-
-}
-
-function ajaxGet(url, callback) {
-  var req = new XMLHttpRequest();
-  req.open("GET", url);
-  req.addEventListener("load", function ()
-  {
-    if (req.status >= 200 && req.status < 400) 
+       
+    req.addEventListener("error", function () 
     {
-            // Appelle la fonction callback en lui passant la réponse de la requête
-            callback(req.responseText);
-    } else { console.error(req.status + " " + req.statusText + " " + url); }
-  });
-   
-  req.addEventListener("error", function () 
-  {
-        console.error("Erreur réseau avec l'URL " + url);
-  });
-  req.send(null);
+      console.error("Erreur réseau avec l'URL " + url);
+    });
+      req.send(null);
+    }
+
+  
+
+
+
 }
+
