5a86f3f3170e167b1f154ebec84a3174bc9303eb
BaptisteC-prog <69847031+BaptisteC-prog@users.noreply.github.com>
2020-10-25 16:21:01 +0100

    Add files via upload

--- js/start.js
+++ js/start.js
@@ -8,19 +8,39 @@ let minGrade=$("#restoGrade").val();
 
 function updateList(){
   let restos=carte.restosInView(carte.listRestos,carte.map.getBounds());
-  
+  //carte.needRefresh=true; 
   $('#content').html("");
   for( let i=0;i<restos.length;i++){
     carte.restoCard(restos[i],i);
   }
+
+  //RESTOS VENANT DE GOOGLE PLACES
+  carte.getRestosFromPlaces(carte.map.getCenter());
+
 }
 
-document.getElementById("restoGrade").onchange = function() {carte.needRefresh=true;};
+document.getElementById("restoGrade").onchange = function() {carte.needRefresh=true;}
 
-google.maps.event.addListener(carte.map, 'click', function() {
-    updateList();
+google.maps.event.addListener(carte.map, 'mousemove', function() {
+  //debug only
 });
 
+setTimeout(function(){
+  updateList();
+}, 300);
+
+carte.map.addListener("click", (event) => {
+  if(carte.context==="addresto") {
+    
+  carte.addMarkerResto(event.latLng);
+  carte.needRefresh=true;
+ //console.log(event.latLng);
+  }
+  carte.context="";
+  
+  updateList();
+
+});
 
 google.maps.event.addListener(carte.map, 'bounds_changed', function() {
   if (carte.fullyLoaded) { updateList(); }
@@ -31,8 +51,10 @@ google.maps.event.addListener(carte.map, 'idle', function() {
 });
 
 $("#restoGradeButton").on("click", function(){
-  
+  carte.deleteMarkers();
+  //this.needRefresh=true; 
   updateList();
+  
 });
 
 
@@ -41,12 +63,24 @@ $("#content").on("click",'.restoCommButton', function(){
  let id_=Number(id);
  let stars=Number($("#gradeID"+id).val());
   carte.addComment(id_,stars,$("#commID"+id).val());
- console.log(carte.listRestos[0]);
+ //console.log(carte.listRestos[0]);
+ this.needRefresh=true; 
   updateList();
 });
 
 
 $("#newRestoName").on("change", function(){
   
-  carte.readyToAdd=true;
+ // carte.context="addresto";
 });
+
+$("#newRestoNameButton").on("click", function(){
+  
+  carte.context="addresto";
+ });
+
+ $("#restoNearby").on("click", function(){
+  carte.getRestosFromPlaces();
+  
+ });
+
