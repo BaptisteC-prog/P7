b74faaa8f51ac218e6815a400dc3475575804fc5
BaptisteC-prog <69847031+BaptisteC-prog@users.noreply.github.com>
2020-10-25 16:20:34 +0100

    Add files via upload

---
+++ js/Restaurant.js
@@ -0,0 +1,15 @@
+"use strict";
+
+ class Restaurant{
+  constructor(name,lat,long)
+  {
+    this.restaurantName=name; 
+    this.address=""; 
+    this.lat=lat;
+    this.long=long;
+    this.ratings=[];
+  }
+
+
+  
+ }
