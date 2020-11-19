1a44e3637f8b6e7dd15b346eb2be56f75946696a
BaptisteC-prog <69847031+BaptisteC-prog@users.noreply.github.com>
2020-11-19 10:25:22 +0100

    Delete apiKey.js

--- js/apiKey.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-    $("body").append("qsdf");  
-  //  alert("<script defer async src='https://maps.googleapis.com/maps/api/js?key=AIzaSyBppDrTKrG2bmb3PH1OTqC3So8m4cz4Wjc&region=FR&language=fr&callback=initMap'</script>");
-$("#script").html("<script src='https://maps.googleapis.com/maps/api/js?key=AIzaSyBppDrTKrG2bmb3PH1OTqC3So8m4cz4Wjc&region=FR&language=fr&callback=initMap'></script>");
-});
